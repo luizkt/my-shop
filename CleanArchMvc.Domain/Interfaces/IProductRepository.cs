28688c7394be5bcdd992d4ef94bdb4e859d288a2
luizktvia <luiz.tabacow@viavarejo.com.br>
2021-07-01 10:10:56 -0300

    Criado base de projeto e Domain

---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetCategoriesAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
