cf2bfa0827600434c00d3a54eeece72a354dc712
luizktvia <luiz.tabacow@viavarejo.com.br>
2021-07-02 16:01:03 -0300

    Adicionado camada de infraestrutura

---
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using CleanArchMvc.Infra.Data.Repositories;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+             b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services; 
+        }
+    }
+}
